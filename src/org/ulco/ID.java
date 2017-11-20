e73ec23cdd748acc13fc8879d41b273fcf617ae3
CamilleINGOUF <ingouf.camille@gmail.com>
2017-11-20 10:21:46 +0100

    Fourth task done

--- src/org/ulco/ID.java
+++ src/org/ulco/ID.java
@@ -1,5 +1,26 @@
 package org.ulco;
 
 public class ID {
-    static public int ID = 0;
+    private int id;
+
+    private static ID instance;
+
+    private ID()
+    {
+        id = 0;
+    }
+
+    public static ID getInstance()
+    {
+        if(instance == null)
+            instance = new ID();
+        else
+            instance.id++;
+        return instance;
+    }
+
+    public int getID()
+    {
+        return id;
+    }
 }
