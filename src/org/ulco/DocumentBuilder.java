066999d8f7e98bd0b81b7cf7bb192c7cd9cf7dd7
CamilleINGOUF <ingouf.camille@gmail.com>
2017-11-20 11:29:16 +0100

    Seventh task done

---
+++ src/org/ulco/DocumentBuilder.java
@@ -0,0 +1,34 @@
+package org.ulco;
+
+/**
+ * Created by cingouf on 20/11/17.
+ */
+public class DocumentBuilder {
+    public static Document BuildDocument(Point origin, int line, int column, double length)
+    {
+        Document document = new Document();
+
+        Layer layer = document.createLayer();
+
+        for (int indexX = 0; indexX < column; ++indexX) {
+            for (int indexY = 0; indexY < line; ++indexY) {
+                layer.add(new Square(new Point(origin.getX() + indexX * length, origin.getY() + indexY * length), length));
+            }
+        }
+
+        return document;
+    }
+
+    public static Document BuildDocument(Point center, int number, double radius, double delta)
+    {
+        Document document = new Document();
+
+        Layer layer = document.createLayer();
+
+        for (int index = 0; index < number; ++index) {
+            layer.add(new Circle(center, radius + index * delta));
+        }
+
+        return document;
+    }
+}
